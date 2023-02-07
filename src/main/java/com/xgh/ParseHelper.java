64219649901a8a5c2ff76d4b1cf384e71e9bdce8
xgh <xieguanghua@fjhb.com>
2023-02-07 14:04:49 +0800

    将当前类转化成proto格式

--- src/main/java/com/xgh/ParseHelper.java
+++ src/main/java/com/xgh/ParseHelper.java
@@ -9,99 +9,144 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * @author xgh 2023/2/6
  */
 public class ParseHelper {
-    private static final Map<String, String> BASIC_TYPE_MAPPING = new HashMap<>();
+    private static final Map<String, String> REFERENCE_MAP = new HashMap<>();
+    private static final Map<String, String> PRIMITIVE_MAP = new HashMap<>();
 
     private static final String CLASS_PREFIX = "message";
     private static final String ENUM_PREFIX = "enum";
-    private static final String STRING_WRAPPER = "google.protobuf.StringValue";
-    private static final String OBJECT_WRAPPER = "google.protobuf.Any";
-    private static final String STRING_NAME = String.class.getName();
     private static final Pattern NULLABLE_PATTERN = Pattern.compile("^@(\\w+\\.)*Nullable$");
+    private static final String GENERIC_TYPE = "google.protobuf.Any";
 
     static {
-        BASIC_TYPE_MAPPING.put("int", "int32");
-        BASIC_TYPE_MAPPING.put(Integer.class.getName(), "google.protobuf.Int32Value");
-        BASIC_TYPE_MAPPING.put("long", "int64");
-        BASIC_TYPE_MAPPING.put(Long.class.getName(), "google.protobuf.Int64Value");
-        BASIC_TYPE_MAPPING.put("double", "double");
-        BASIC_TYPE_MAPPING.put(Double.class.getName(), "google.protobuf.DoubleValue");
-        BASIC_TYPE_MAPPING.put("short", "int32");
-        BASIC_TYPE_MAPPING.put(Short.class.getName(), "google.protobuf.Int32Value");
-        BASIC_TYPE_MAPPING.put("char", "string");
-        BASIC_TYPE_MAPPING.put(Character.class.getName(), "google.protobuf.StringValue");
-        BASIC_TYPE_MAPPING.put("boolean", "bool");
-        BASIC_TYPE_MAPPING.put(Boolean.class.getName(), "google.protobuf.BoolValue");
-        BASIC_TYPE_MAPPING.put("float", "float");
-        BASIC_TYPE_MAPPING.put(Float.class.getName(), "google.protobuf.FloatValue");
-        BASIC_TYPE_MAPPING.put("byte", "bytes");
-        BASIC_TYPE_MAPPING.put(Byte.class.getName(), "google.protobuf.BytesValue");
-        BASIC_TYPE_MAPPING.put(STRING_NAME, "string");
-        BASIC_TYPE_MAPPING.put(Date.class.getName(), "google.protobuf.Timestamp");
-        BASIC_TYPE_MAPPING.put(LocalDate.class.getName(), "google.protobuf.Timestamp");
-        BASIC_TYPE_MAPPING.put(LocalDateTime.class.getName(), "google.protobuf.Timestamp");
+        REFERENCE_MAP.put(Byte.class.getName(), "google.protobuf.BytesValue");
+        REFERENCE_MAP.put(Boolean.class.getName(), "google.protobuf.BoolValue");
+        REFERENCE_MAP.put(Character.class.getName(), "google.protobuf.StringValue");
+        REFERENCE_MAP.put(Short.class.getName(), "google.protobuf.Int32Value");
+        REFERENCE_MAP.put(Integer.class.getName(), "google.protobuf.Int32Value");
+        REFERENCE_MAP.put(Long.class.getName(), "google.protobuf.Int64Value");
+        REFERENCE_MAP.put(Float.class.getName(), "google.protobuf.FloatValue");
+        REFERENCE_MAP.put(Double.class.getName(), "google.protobuf.DoubleValue");
+        REFERENCE_MAP.put(String.class.getName(), "google.protobuf.StringValue");
+        REFERENCE_MAP.put(Date.class.getName(), "google.protobuf.Timestamp");
+        REFERENCE_MAP.put(LocalDate.class.getName(), "google.protobuf.Timestamp");
+        REFERENCE_MAP.put(LocalDateTime.class.getName(), "google.protobuf.Timestamp");
+        REFERENCE_MAP.put(Object.class.getName(), "google.protobuf.Any");
+
+
+        PRIMITIVE_MAP.put("int", "int32");
+        PRIMITIVE_MAP.put("long", "int64");
+        PRIMITIVE_MAP.put("double", "double");
+        PRIMITIVE_MAP.put("short", "int32");
+        PRIMITIVE_MAP.put("char", "string");
+        PRIMITIVE_MAP.put("boolean", "bool");
+        PRIMITIVE_MAP.put("float", "float");
+        PRIMITIVE_MAP.put("byte", "bytes");
+
     }
 
 
-    public static List<List<String>> parse(PsiClass psiClass, boolean asBasic) {
-        List<List<String>> list = new ArrayList<>();
-        if (psiClass.isEnum()) {
-            list.add(parseEnum(psiClass, ""));
-        } else {
-            if (psiClass.isInterface()) {
-                list.add(Collections.singletonList("// interface 不能转换"));
-            } else {
-                list.add(parseClass(psiClass, "", asBasic));
+    public static List<String> parse(PsiClass psiClass) {
+        List<String> list = new ArrayList<>();
+        List<PsiClass> classes = getAllClasses(psiClass);
+        classes.forEach(
+                clazz -> {
+                    if (clazz.isEnum()) {
+                        list.addAll(parseEnum(clazz, ""));
+                    } else {
+                        if (clazz.isInterface()) {
+                            list.add("// interface 不能转换");
+                        } else {
+                            list.addAll(parseClass(clazz, ""));
+                        }
+                    }
+                }
+        );
+
+        return list;
+    }
+
+    private static List<PsiClass> getAllClasses(PsiClass psiClass) {
+        ArrayList<PsiClass> list = new ArrayList<>();
+        list.add(psiClass);
+        PsiClass[] innerClasses = psiClass.getInnerClasses();
+        if (innerClasses.length > 0) {
+            for (PsiClass innerClass : innerClasses) {
+                list.addAll(getAllClasses(innerClass));
             }
         }
         return list;
     }
 
-    private static List<String> parseClass(PsiClass psiClass, String indent, boolean asBasic) {
+    private static List<String> parseClass(PsiClass psiClass, String indent) {
         List<String> list = new ArrayList<>();
-        PsiDocComment classComment = psiClass.getDocComment();
-        if (classComment != null) {
-            list.add(classComment.getText());
-        }
+        Set<String> genericTypes = Arrays.stream(psiClass.getTypeParameters()).map(PsiTypeParameter::getName).collect(Collectors.toSet());
+        addDoc(psiClass, indent, list);
         list.add(indent + CLASS_PREFIX + " " + psiClass.getName() + " {");
         PsiField[] fields = psiClass.getFields();
         String fieldIndent = indent + "\t";
-        int index = 0;
+        int index = 1;
         for (int i = 0; i < fields.length; i++) {
             PsiField field = fields[i];
             if (field.hasModifier(JvmModifier.STATIC) || field.hasModifier(JvmModifier.FINAL)) {
                 continue;
             }
-            if (asBasic || BASIC_TYPE_MAPPING.containsKey(field.getType().getCanonicalText())) {
-
-                list.add(String.format(copeFieldAsBasicType(field, fieldIndent), index++));
-            } else {
-                list.addAll(copeObjectTypeField(field, fieldIndent));
+            String type = getProtoType(field.getType(), genericTypes);
+            if (type.contains("google.protobuf.StringValue")) {
+                if (!hasNullableAnnotation(field)) {
+                    type = type.replace("google.protobuf.StringValue", "string");
+                }
             }
+            addDoc(field, indent, list);
+            list.add(fieldIndent + type + " " + field.getName() + " = " + index++ + ";");
         }
         list.add("}");
         return list;
     }
 
-    private static String copeFieldAsBasicType(PsiField field, String indent) {
-        String type = "";
-        String typeName = field.getType().getCanonicalText();
-        if (STRING_NAME.equals(typeName) && hasNullableAnnotation(field)) {
-            type = STRING_WRAPPER;
-        } else if (BASIC_TYPE_MAPPING.containsKey(typeName)) {
-            type = BASIC_TYPE_MAPPING.get(typeName);
-        } else if (Object.class.getName().equals(typeName)) {
-            type = OBJECT_WRAPPER;
-        } else if (field.getType() instanceof PsiClassReferenceType) {
-            PsiJavaCodeReferenceElement reference = ((PsiClassReferenceType) field.getType()).getReference();
-            String className = reference.getQualifiedName();
-            type = field.getType().getPresentableText();
+    private static String getProtoType(PsiType type, Set<String> genericType) {
+        if (type instanceof PsiClassReferenceType) {
+            PsiClassReferenceType pt = (PsiClassReferenceType) type;
+            String className = pt.getReference().getQualifiedName();
+            if (REFERENCE_MAP.containsKey(className)) {
+                return REFERENCE_MAP.get(className);
+            }
+            Class jClass = isPresent(className);
+            if (jClass != null) {
+                if (Collection.class.isAssignableFrom(jClass)) {
+                    if (pt.getParameters().length > 0) {
+                        return "repeated " + getProtoType(pt.getParameters()[0], genericType);
+                    } else {
+                        return "repeated " + GENERIC_TYPE;
+                    }
+                }
+                if (Map.class.isAssignableFrom(jClass)) {
+                    if (pt.getParameters().length > 0) {
+                        String keyProtoType = getProtoType(pt.getParameters()[0], genericType);
+                        String valProtoType = getProtoType(pt.getParameters()[1], genericType);
+                        return String.format("map<%s, %s>", keyProtoType, valProtoType);
+                    } else {
+                        return String.format("map<%s, %s>", GENERIC_TYPE, GENERIC_TYPE);
+                    }
+                }
+            }
+            if (genericType.contains(className)) {
+                return GENERIC_TYPE;
+            }
+
+            return className.substring(className.lastIndexOf(".") + 1);
+        } else if (type instanceof PsiPrimitiveType) {
+            return PRIMITIVE_MAP.get(((PsiPrimitiveType) type).getName());
+        } else if (type instanceof PsiArrayType) {
+            return "repeated " + getProtoType(((PsiArrayType) type).getComponentType(), genericType);
+        } else {
+            throw new IllegalArgumentException("未知的类型:" + type.getCanonicalText());
         }
-        return indent + type + " " + field.getName() + " = %s";
     }
 
     private static boolean hasNullableAnnotation(PsiField field) {
@@ -117,22 +162,28 @@ public class ParseHelper {
         return false;
     }
 
-    private static List<String> copeObjectTypeField(PsiField psiField, String indent) {
-
-
-        return null;
+    private static Class isPresent(String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
     }
 
-    private void addDoc(PsiJavaDocumentedElement element, List<String> list) {
+
+    private static void addDoc(PsiJavaDocumentedElement element, String indent, List<String> list) {
         PsiDocComment comment = element.getDocComment();
         if (comment != null) {
-            list.add(comment.getText());
+            String commentText = comment.getText();
+            String s = Arrays.stream(commentText.split("\n")).map(String::trim).map(str -> indent + str).collect(Collectors.joining("\n "+indent));
+            list.add(s);
         }
     }
 
 
     private static List<String> parseEnum(PsiClass psiClass, String indent) {
         List<String> list = new ArrayList<>();
+        addDoc(psiClass, indent, list);
         list.add(indent + ENUM_PREFIX + " " + psiClass.getName() + " {");
         PsiField[] fields = psiClass.getFields();
         for (int i = 0; i < fields.length; i++) {
--- src/main/java/com/xgh/ParseOneAction.java
+++ src/main/java/com/xgh/ParseOneAction.java
@@ -7,28 +7,45 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.datatransfer.StringSelection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author xgh 2023/2/6
  */
 public class ParseOneAction extends AnAction {
 
+    public static final NotificationGroup NG = new NotificationGroup(ParseOneAction.class.getSimpleName(), NotificationDisplayType.BALLOON);
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         PsiFile psiFile = event.getData(CommonDataKeys.PSI_FILE);
         PsiClass psiClass = PsiTreeUtil.getChildOfType(psiFile, PsiClass.class);
+        Notification notification = null;
+        try {
+            List<String> parse = ParseHelper.parse(psiClass);
+            copyToClipboard(parse);
+            notification = NG.createNotification("生成成功已复制到剪切版", MessageType.INFO);
+        } catch (Exception e) {
+            notification = NG.createNotification("生成异常:" + e.getMessage(), MessageType.ERROR);
+        } finally {
+            Notifications.Bus.notify(notification);
+        }
+    }
 
-        List<List<String>> parse = ParseHelper.parse(psiClass, true);
-        parse.get(0).forEach(System.out::println);
-        System.out.println(psiClass);
-        Notification notification = new NotificationGroup("test", NotificationDisplayType.BALLOON).createNotification("hello world", MessageType.ERROR);
-        Notifications.Bus.notify(notification);
+    public void copyToClipboard(String content) {
+        CopyPasteManager.getInstance().setContents(new StringSelection(content));
+    }
+
+    public void copyToClipboard(List<String> list) {
+        copyToClipboard(list.stream().collect(Collectors.joining("\n")));
     }
 }
